a53fa37d023471411c9ddde04e3add8606cb928b
Sam <167071477+SkillfulRheyme4@users.noreply.github.com>
2024-06-24 11:32:46 +0200

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,1 +1,15 @@
+#include "IMU.h"
+#include "XBeeCommunicator.h"
+
+XBeeCommunicator xbeeCommunicator(Serial1);
+IMU imu(xbeeCommunicator);
+
+void setup() {
+  xbeeCommunicator.begin(9600);
+  imu.init();
+}
+
+void loop() {
+  imu.sendIMUReport();
+}
 
