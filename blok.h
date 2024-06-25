e2f9eeee0ff7065b5d3ab7bb3aaefd54c25fa3f9
Sem De Carvalho pinto <65860384+cilio0@users.noreply.github.com>
2024-06-25 11:32:34 +0200

    Update blok.h

--- blok.h
+++ blok.h
@@ -3,15 +3,18 @@
 
 #include "MyZumo32U4Proximity.h"
 #include "XBeeCommunicator.h"
+#include <Zumo32U4Motors.h>
+#include <Zumo32U4Buttons.h> // Include Zumo32U4Buttons header
 
 // Forward declaration of Zumo32U4Motors class
 class Zumo32U4Motors;
 
 const uint8_t sensorThreshold = 1;
-const uint16_t turnSpeedMax = 400;
+const uint16_t turnSpeedMax = 150;
 const uint16_t turnSpeedMin = 100;
 const uint16_t deceleration = 10;
 const uint16_t acceleration = 10;
+const uint16_t forwardSpeed = 200; // Speed for moving forward
 
 #define LEFT 0
 #define RIGHT 1
@@ -25,9 +28,11 @@ public:
     void turnLeft();
     void stop();
     void loop();
+    void scan360();
+    void moveForward(); // Declaration of the moveForward function
 
 private:
-    Zumo32U4Motors motors; // Now only declared, not included
+    Zumo32U4Motors motors;
     Zumo32U4ButtonA buttonA;
     MyZumo32U4Proximity proxSensors;
     XBeeCommunicator xbee;
