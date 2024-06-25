f8c9043aa26322aeb5c9b9d190980b50d8e411b7
HHS23052236 <149047348+HHS23052236@users.noreply.github.com>
2024-06-25 11:42:36 +0200

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,1 +1,24 @@
+#include <Wire.h>
+#include "BuzzerController.h"
+#include "LedController.h"
+#include "BatteryController.h"
+#include "XBeeCommunicator.h"
 
+// De Serial1 verbinding wordt hier verklaard naar xbee
+#define xbee Serial1
+
+BuzzerController buzzerController;
+LedController ledController;
+BatteryController batteryController;
+XBeeCommunicator xbeeCommunicator(xbee);
+
+void setup() {
+    xbee.begin(9600);
+    ledController.startLed();
+    batteryController.setXBeeCommunicator(&xbeeCommunicator);
+}
+
+void loop() {
+    // Check de batterijstatus en print naar xbee
+    batteryController.checkBattery();
+}
