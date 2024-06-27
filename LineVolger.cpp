a7ce95dcd17e1df5fbaf5b737779afcb45146280
Len2w2 <168977585+Len2w2@users.noreply.github.com>
2024-06-27 12:57:00 +0200

    Update LineVolger.cpp

--- LineVolger.cpp
+++ LineVolger.cpp
@@ -1,33 +1,41 @@
 #include "LineVolger.h"
 
-LineVolger::LineVolger(Motors& motors) : rij(motors), lastError(0) {}
-
-void LineVolger::setupMotors() {
-    buttonA.waitForButton();
+void LineVolger::setup() {
+    lastError = 0;
 }
 
-void LineVolger::controlMotors(int16_t speedDifference) {
-    int links = (int16_t)Motors::getMaxSpeed() + speedDifference;
-    int rechts = (int16_t)Motors::getMaxSpeed() - speedDifference;
-
-    links = constrain(links, 0, (int16_t)Motors::getMaxSpeed());
-    rechts = constrain(rechts, 0, (int16_t)Motors::getMaxSpeed());
-
-    rij.getMotors().setSpeeds(links, rechts);
+void LineVolger::calibrateSensors() {
+    lineSensor.calibrateSensors(motors.getMotors());
 }
 
-int16_t LineVolger::getLastError() const {
-    return lastError;
+void LineVolger::loop() {
+    followLine();
 }
 
-void LineVolger::setLastError(int16_t error) {
+void LineVolger::followLine() {
+    int16_t position = lineSensor.readLineSensor();
+
+    // Onze "error" is hoe ver we van het midden van de lijn verwijderd zijn,
+    // wat overeenkomt met positie 2000.
+    int16_t error = position - 2000;
+
+    computeAndSetSpeeds(error, lastError);
+
     lastError = error;
 }
 
-void LineVolger::readSensors() {
-    int16_t position = lineSensor.readLineSensor(); // Use the LineSensor instance to read the position
-    int16_t error = position - 2000;
-    int16_t speedDifference = error / 0.75 + 20 * (error - getLastError());
-    setLastError(error);
-    controlMotors(speedDifference);
+void LineVolger::computeAndSetSpeeds(int16_t error, int16_t lastError) {
+    int16_t speedDifference = error / 0.75 + 20 * (error - lastError);
+
+    // Verkrijg individuele motorsnelheden. Het teken van speedDifference
+    // bepaalt of de robot naar links of naar rechts draait.
+    int16_t leftSpeed = motors.maxSpeed + speedDifference;
+    int16_t rightSpeed = motors.maxSpeed - speedDifference;
+
+    // Beperk onze motorsnelheden tot tussen 0 en maxSpeed.
+    // Hierdoor draait hij niet om nadat hij wit of een andere kleur gemeten heeft.
+    leftSpeed = constrain(leftSpeed, -100, motors.maxSpeed);
+    rightSpeed = constrain(rightSpeed, -100, motors.maxSpeed);
+
+    motors.setSpeeds(leftSpeed, rightSpeed);
 }
