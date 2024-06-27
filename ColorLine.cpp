24f4742f0400b8e715d6123d877088b7dae438e4
Okemagdit <114150647+Okemagdit@users.noreply.github.com>
2024-06-27 10:24:16 +0200

    Update ColorLine.cpp

--- ColorLine.cpp
+++ ColorLine.cpp
@@ -1,80 +1,154 @@
 #include "ColorLine.h"
 
-ColorLine::ColorLine(int L, int M, int R) : iets(L), iets1(M), iets2(R)// construeren van de int naar iets toe
+ColorLine::ColorLine(int L, int M, int R)
+  : iets(L), iets1(M), iets2(R)  // construeren van de int naar iets toe
 {
-
 }
 
-bool ColorLine::isWithinRange(int value,int lowerBound,int upperBound){
+bool ColorLine::isWithinRange(int value, int lowerBound, int upperBound) {
   return value > lowerBound && value < upperBound;
 }
 
-void ColorLine::Color(){ // het kiezen in welke kleur het zicht bevind
-  
-    if (isWithinRange(iets, 50, 200) && isWithinRange(iets1, 50, 200) && isWithinRange(iets2, 50, 150)) {//if else statement becaus an switch statement can't do number range with in the statement
-        // if this is true then the color is Green
-        Groen = true;
-        Grijsrecht = false;
-        GrijsLinks = false;
-        Zwart = false;
-        niks = false;
-    } else if (isWithinRange(iets, 200, 400)) {
-        // if this is true then the color is Gray left and we need to go left
-        Groen = false;
-        Grijsrecht = false;
-        GrijsLinks = true;
-        Zwart = false;
-        niks = false;
-    } else if (isWithinRange(iets2, 150, 250)) {
-        // if this is true then the color is Gray right and we need to go right
-        Groen = false;
-        Grijsrecht = true;
-        GrijsLinks = false;
-        Zwart = false;
-        niks = false;
-    } else if (isWithinRange(iets, 900, 1000) && isWithinRange(iets1, 900, 1000) && isWithinRange(iets2, 900, 1000)) {
-        // if this is true then the color is Black
-        Groen = false;
-        Grijsrecht = false;
-        GrijsLinks = false;
-        Zwart = true;
-        niks = false;
-    } else {
-        // if none of the above is true then the color is nothing
-        Groen = false;
-        Grijsrecht = false;
-        GrijsLinks = false;
-        Zwart = false;
-        niks = true;
-    }
+void ColorLine::Color() {  // het kiezen in welke kleur het zicht bevind
 
+  if (isWithinRange(iets, 50, 200)) {  //if else statement becaus an switch statement can't do number range with in the statement
+    // if this is true then the color is Green
+    niks2 = '1';
 
+  } else if (isWithinRange(iets, 200, 400)) {
+    // if this is true then the color is Gray left and we need to go left
+    niks2 = '2';
+
+    //} else if (isWithinRange(iets2, 150, 250)) {
+    // if this is true then the color is Gray right and we need to go right
+    // niks2 = '3';
+
+  } else if (isWithinRange(iets, 900, 1000)) {
+    // if this is true then the color is Black
+    niks2 = '4';
+
+  } else {
+    // if none of the above is true then the color is nothing
+    niks2 = '5';
+  }
+
+  if (isWithinRange(iets1, 50, 200)) {  //if else statement becaus an switch statement can't do number range with in the statement
+    // if this is true then the color is Green
+    niks3 = '1';
+
+    //} else if (isWithinRange(iets, 200, 400)) {
+    // if this is true then the color is Gray left and we need to go left
+    //  niks3 = '2';
+
+    //} else if (isWithinRange(iets2, 150, 250)) {
+    // if this is true then the color is Gray right and we need to go right
+    // niks3 = '3';
+
+  } else if (isWithinRange(iets1, 900, 1000)) {
+    // if this is true then the color is Black
+    niks3 = '4';
+
+  } else {
+    // if none of the above is true then the color is nothing
+    niks3 = '5';
+  }
+
+  if (isWithinRange(iets2, 50, 150)) {  //if else statement becaus an switch statement can't do number range with in the statement
+    // if this is true then the color is Green
+    niks4 = '1';
+
+    // } else if (isWithinRange(iets, 200, 400)) {
+    // if this is true then the color is Gray left and we need to go left
+    // niks4 = '2';
+
+  } else if (isWithinRange(iets2, 150, 250)) {
+    // if this is true then the color is Gray right and we need to go right
+    niks4 = '3';
+
+  } else if (isWithinRange(iets2, 900, 1000)) {
+    // if this is true then the color is Black
+    niks4 = '4';
+
+  } else {
+    // if none of the above is true then the color is nothing
+    niks4 = '5';
+  }
 }
 
-String ColorLine::kleure(){ // Geeft mee in een string welke kleur er actief is
+String ColorLine::kleure1() {  // Geeft mee in een string welke kleur er actief is
 
-switch(Groen,Grijsrecht,GrijsLinks,Zwart,niks){
+  switch (niks2) {
 
- case 'Groen':
-   return "Groen";
- break;
+    case '1':
+      return "Groen";
+      break;
 
- case 'Grijsrecht':
-   return "Grijsrecht";
- break;
+      //case '2':
+      //return "Grijsrecht";
+      //break;
 
- case 'GrijsLinks':
-   return "GrijsLinks";
- break;
+    case '3':
+      return "GrijsLinks";
+      break;
 
- case 'Zwart':
-   return "Zwart";
- break;
+    case '4':
+      return "Zwart";
+      break;
 
- case 'niks':
-   return "niks";
- break;
-
+    case '5':
+      return "niks";
+      break;
+  }
 }
 
+String ColorLine::kleure2() {  // Geeft mee in een string welke kleur er actief is
+
+  switch (niks3) {
+
+    case '1':
+      return "Groen";
+      break;
+
+      //case '2':
+      //return "Grijsrecht";
+      //break;
+
+      //case '3':
+      //return "GrijsLinks";
+      //break;
+
+    case '4':
+      return "Zwart";
+      break;
+
+    case '5':
+      return "niks";
+      break;
+  }
+}
+
+String ColorLine::kleure3() {  // Geeft mee in een string welke kleur er actief is
+
+  switch (niks4) {
+
+    case '1':
+      return "Groen";
+      break;
+
+    case '2':
+      return "Grijsrecht";
+      break;
+
+      //case '3':
+      //return "GrijsLinks";
+      //break;
+
+    case '4':
+      return "Zwart";
+      break;
+
+    case '5':
+      return "niks";
+      break;
+  }
 }
